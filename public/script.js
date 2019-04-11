85564da13a1ae528ae64870ea6d1295faa0fea47
Daniel Buchanan <buchanandjb@gmail.com>
2019-04-11 15:48:15 -0600

    this is my first commit

---
+++ public/script.js
@@ -0,0 +1,23 @@
+Vue.component('star-rating', VueStarRating.default);
+
+// APP VUE //
+var app = new Vue({
+  el: '#app',
+  data: {
+    items: [],
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
+});
